2a6e74ddac61d2576b5621c8c84a0b11535172f8
pirvudoru <pirvu.doru@ymail.com>
2022-04-16 16:52:02 +0300

    Make specs pass

--- projects/02-meme-generator-api/implementation/app/controllers/application_controller.rb
+++ projects/02-meme-generator-api/implementation/app/controllers/application_controller.rb
@@ -47,20 +47,20 @@ class ApplicationController < Sinatra::Base
 
   post '/signup' do
     begin
-      RequestBodyValidator.validate(@request_body_json)
-    rescue RequestBodyValidatorError => e
+      @username = @request_body_json['user']['username']
+      @password = @request_body_json['user']['password']
+    rescue
       status 400
-      body e
     else
-     # if @username.empty? # TODO: Introduce a validator class
-      #  @blank_username_error = { 'errors': [{ 'message': 'Username is blank' }] } # TODO: Extract error serialization to avoid duplication
-      #  status 400
-      #  @blank_username_error.to_json
-      #elsif @password.empty?
-      #  @blank_password_error = { 'errors': [{ 'message': 'Password is blank' }] }
-      #  status 400
-      #  body @blank_password_error.to_json
-      #else
+     if @username.empty? # TODO: Introduce a validator class
+       @blank_username_error = { 'errors': [{ 'message': 'Username is blank' }] } # TODO: Extract error serialization to avoid duplication
+       status 400
+       @blank_username_error.to_json
+      elsif @password.empty?
+       @blank_password_error = { 'errors': [{ 'message': 'Password is blank' }] }
+       status 400
+       body @blank_password_error.to_json
+      else
         begin
           @user_token = AuthenticationClient.create_user(@username, @password) # TODO: Look at ServiceObject pattern
         rescue UserAlreadyExistsError
@@ -69,7 +69,7 @@ class ApplicationController < Sinatra::Base
           status 201
           body @user_token
         end
-      #end
+      end
     end
   end
 
